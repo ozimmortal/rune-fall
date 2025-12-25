b5526599e043d6956c168573eb4e9d5d363e765e
oz <zaleambo@gmail.com>
2025-12-25 22:58:43 +0300

    LEVEL 1 COMPLETE Add default audio bus layout and music scene
    - Created a default bus layout with two buses: Music and SFX.
    - Configured the Music bus with a volume of 1.456 and the SFX bus with a volume of 0.0.
    - Added a new music scene that utilizes an AudioStreamPlayer2D to play "time_for_adventure.mp3" on the Music bus.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,11 +3,11 @@ extends Area2D
 
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		game_manager.add_point()
-		
-		queue_free()
+		animation_player.play("pickup")
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,9 +3,13 @@ extends Node
 
 var C = 0
 @onready var score: Label = %score
+
+func _ready() -> void:
+	score.text = "You have collected" + str(C) + "coins."
+	
 func add_point():
 	C += 1
 	score.text = "You have collected" + str(C) + "coins."
-	print(score)
+	print(C)
 	
 	
