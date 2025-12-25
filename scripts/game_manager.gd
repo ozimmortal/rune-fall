4daeb2567380d49149fef0de4eb195da8d5ef201
oz <zaleambo@gmail.com>
2025-12-25 23:05:18 +0300

    Fix spacing in score display messages for improved readability

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,11 +5,11 @@ var C = 0
 @onready var score: Label = %score
 
 func _ready() -> void:
-	score.text = "You have collected" + str(C) + "coins."
+	score.text = "You have collected " + str(C) + " coins."
 	
 func add_point():
 	C += 1
-	score.text = "You have collected" + str(C) + "coins."
+	score.text = "You have collected " + str(C) + " coins."
 	print(C)
 	
 	
